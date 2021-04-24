d534b8f35e8abe330a6e8f669a198f4b1d0158eb
Swapnil Sharma <nilinswap@gmail.com>
2021-04-24 13:11:17 +0530

    to make it work

--- src/components/Root/Root.types.ts
+++ src/components/Root/Root.types.ts
@@ -1,4 +1,4 @@
-import { ArtifactType } from "~/App/App.types"
+import { ArtifactType } from "../../App/App.types"
 
 export interface RootProps {
   artifacts: Array<ArtifactType> // dummy argument
